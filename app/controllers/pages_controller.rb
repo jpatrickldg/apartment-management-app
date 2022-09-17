8c4fd88b084299dea01826fdd9a5d6c6162b3934
jpatrickldg <jpatrickldg@gmail.com>
2022-09-17 14:31:35 +0800

    Add Inquiry Form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def new_inquiry
+    @new_inquiry = Inquiry.new
+  end
+
+  def create_inquiry
+    @new_inquiry = Inquiry.new(inquiry_params)
+    if @new_inquiry.save!
+      redirect_to root_path, notice: 'Inquiry Added'
+    else
+      render :new_inquiry
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:email, :first_name, :last_name, :gender, :contact_no, :occupation, :location_preference, :room_type, :move_in_date )
+  end
+
 end
