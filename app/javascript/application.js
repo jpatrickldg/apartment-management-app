46e8ac84c46797098386843f9e95175e3d1fddc1
jpatrickldg <jpatrickldg@gmail.com>
2022-10-06 13:32:51 +0800

    add menu icon for mobile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "controllers"
 import "@fortawesome/fontawesome-free"
 
 import "chartkick"
 import "Chart.bundle"
+
+let menu = document.querySelector('.menu');
+let sidebar = document.querySelector('aside');
+
+menu.addEventListener('click', () => {
+  sidebar.classList.toggle('-left-64')
+  sidebar.classList.toggle('transition-all')
+  sidebar.classList.toggle('duration-100')
+})
+
