22b141690d100688f3356bde53325e7aae7980ab
jpatrickldg <jpatrickldg@gmail.com>
2022-10-18 10:59:35 +0800

    Add email settings for production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'mwpmi@test.com'
+  default from: ENV['GMAIL']
 
   def due_reminder
     @user = params[:user]
     @booking = @user.bookings.find_by(status: 'active')
-    mail(to: @user.email, subject: 'A gentle payment reminder')
+    mail(to: @user.email, subject: 'A gentle payment reminder for you incoming due date')
   end
 end
