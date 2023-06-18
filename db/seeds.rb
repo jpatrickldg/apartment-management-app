62a2d048b3cd586429a4a5af584a9b41ec7ba218
jpatrickldg <jpatrickldg@gmail.com>
2023-06-18 20:45:57 +0800

    change seed file; update host on environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,145 +5,292 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Payment.destroy_all 
-Invoice.destroy_all 
-Booking.destroy_all 
-Inquiry.destroy_all
-Concern.destroy_all
-Expense.destroy_all
-Announcement.destroy_all
-User.destroy_all
+# Payment.destroy_all 
+# Invoice.destroy_all 
+# Booking.destroy_all 
+# Inquiry.destroy_all
+# Concern.destroy_all
+# Expense.destroy_all
+# Announcement.destroy_all
+# User.destroy_all
 
 
 User.create([
   {
-    email: 'admin@test.com',
-    password: '123456',
-    password_confirmation: '123456',
+    email: 'suamwpmi@gmail.com',
+    password: 'password',
+    password_confirmation: 'password',
     first_name: 'admin',
     last_name: 'admin',
     birthdate: Date.today,
     gender: 'male',
-    contact_no: '09123456789',
-    address: 'Trial Address 1',
+    contact_no: '09087731123',
+    address: 'Sampaloc, Manila',
     role: 'admin',
     occupation: 'admin',
     confirmed_at: Time.now
   },
   {
-    email: 'owner@test.com',
-    password: '123456',
-    password_confirmation: '123456',
+    email: 'ownermwpmi@gmail.com',
+    password: 'password',
+    password_confirmation: 'password',
     first_name: 'owner',
     last_name: 'owner',
     birthdate: Date.today,
     gender: 'male',
-    contact_no: '09213456789',
-    address: 'Trial Address 2',
+    contact_no: '09223392811',
+    address: 'Sampaloc, Manila',
     role: 'owner',
     occupation: 'owner',
     confirmed_at: Time.now
   },
   {
-    email: 'receptionist1@test.com',
-    password: '123456',
-    password_confirmation: '123456',
-    first_name: 'receptionist1',
-    last_name: 'receptionist1',
+    email: 'recmwpmi@gmail.com',
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: 'receptionist',
+    last_name: 'staff',
     birthdate: Date.today,
     gender: 'female',
     contact_no: '09432123213',
-    address: 'Trial Address 3',
+    address: 'Sampaloc, Manila',
     role: 'receptionist',
     occupation: 'staff',
     confirmed_at: Time.now
   },
   {
-    email: 'receptionist2@test.com',
-    password: '123456',
-    password_confirmation: '123456',
-    first_name: 'receptionist2',
-    last_name: 'receptionist2',
-    birthdate: Date.today,
-    gender: 'male',
-    contact_no: '09432123211',
-    address: 'Trial Address 4',
-    role: 'receptionist',
-    occupation: 'staff',
-    confirmed_at: Time.now
-  },
-  {
-    email: 'cashier1@test.com',
-    password: '123456',
-    password_confirmation: '123456',
-    first_name: 'cashier1',
-    last_name: 'cashier1',
-    birthdate: Date.today,
-    gender: 'female',
-    contact_no: '09432123242',
-    address: 'Trial Address 5',
-    role: 'cashier',
-    occupation: 'staff',
-    confirmed_at: Time.now
-  },
-  {
-    email: 'cashier2@test.com',
-    password: '123456',
-    password_confirmation: '123456',
-    first_name: 'cashier2',
-    last_name: 'cashier2',
+    email: 'cashiermwpmi@gmail.com',
+    password: 'password',
+    password_confirmation: 'password',
+    first_name: 'cashier',
+    last_name: 'staff',
     birthdate: Date.today,
     gender: 'male',
     contact_no: '09432123457',
-    address: 'Trial Address 6',
+    address: 'Sampaloc, Manila',
     role: 'cashier',
     occupation: 'staff',
     confirmed_at: Time.now
   }
 ])
 
-5.times do |x|
-  User.create({
-    email: "tenant#{x+1}@test.com",
-    password: '123456',
-    password_confirmation: '123456',
-    first_name: "Tenant#{x+1}",
-    last_name: "Test#{x+1}",
-    birthdate: Date.today,
-    gender: 'male',
-    contact_no: "0912345678#{x}",
-    address: "Trial Address #{x+6}",
-    role: 'tenant',
-    occupation: 'student',
-    emergency_contact_person: "Parent#{x}",
-    emergency_contact_no: "0912545678#{x}",
-    confirmed_at: Time.now
-  })
-end
+# 5.times do |x|
+#   User.create({
+#     email: "tenant#{x+1}@gmail.com",
+#     password: '123456',
+#     password_confirmation: '123456',
+#     first_name: "Tenant#{x+1}",
+#     last_name: "Test#{x+1}",
+#     birthdate: Date.today,
+#     gender: 'male',
+#     contact_no: "0912345678#{x}",
+#     address: "Trial Address #{x+6}",
+#     role: 'tenant',
+#     occupation: 'student',
+#     emergency_contact_person: "Parent#{x}",
+#     emergency_contact_no: "0912545678#{x}",
+#     confirmed_at: Time.now
+#   })
+# end
 
-Branch.create([
+branches = [
   {
     branch_type: 'Boarding House',
-    address: 'Recto Avenue, Sampaloc, Manila'
+    address: 'Recto Avenue, Sampaloc, Manila',
+    rooms: [
+      { 
+        room_code: 'B1R1',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B1R2',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B1R3',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B1R4',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B1R5',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+    ]
   },
   {
     branch_type: 'Condominium - Fully Furnished',
-    address: 'Espana Boulevard, Manila'
+    address: 'Espana Boulevard, Manila',
+    rooms: [
+      { 
+        room_code: 'B2R1',
+        monthly_rate: 12000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B2R2',
+        monthly_rate: 12000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B2R3',
+        monthly_rate: 12000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+    ]
   },
   {
     branch_type: 'Bedspace',
-    address: 'Vicente Cruz St., Sampaloc, Manila'
+    address: 'Vicente Cruz St., Sampaloc, Manila',
+    rooms: [
+      { 
+        room_code: 'B3R1',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B3R2',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B3R3',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B3R4',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B3R5',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+    ]
   },
   {
     branch_type: 'Condominium - Empty',
-    address: 'Tacio St., Sampaloc, Manila'
+    address: 'Tacio St., Sampaloc, Manila',
+    rooms: [
+      { 
+        room_code: 'B4R1',
+        monthly_rate: 10000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B4R2',
+        monthly_rate: 10000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B4R3',
+        monthly_rate: 10000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+    ]
   },
   {
     branch_type: 'Studio',
-    address: 'Maria Cristina St., Sampaloc, Manila'
+    address: 'Maria Cristina St., Sampaloc, Manila',
+    rooms: [
+      { 
+        room_code: 'B5R1',
+        monthly_rate: 8000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B5R2',
+        monthly_rate: 8000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B5R3',
+        monthly_rate: 8000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B5R4',
+        monthly_rate: 8000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+      { 
+        room_code: 'B5R5',
+        monthly_rate: 8000, 
+        occupants_count: 0,
+        capacity_count: 1, 
+      },
+    ]
   },
   {
     branch_type: 'Bedspace',
-    address: 'Cristobal St., Sampaloc, Manila'
-  },
-])
+    address: 'Cristobal St., Sampaloc, Manila',
+    rooms: [
+      { 
+        room_code: 'B6R1',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B6R2',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B6R3',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B6R4',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+      { 
+        room_code: 'B6R5',
+        monthly_rate: 5000, 
+        occupants_count: 0,
+        capacity_count: 8, 
+      },
+    ]
+  }
+]
+
+branches.each do |branch_attrs|
+  branch = Branch.create(branch_attrs.except(:rooms))
+  branch_attrs[:rooms].each do |room_attrs|
+    branch.rooms.create(room_attrs)
+  end
+end
